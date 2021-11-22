df5a17113d57ade826fe2fc29603eb80079e476d
Jivesh Makkar <c0814268@mylambton.ca>
2021-11-22 16:39:05 -0500

    user layout file and UserAdapter class update

--- app/src/main/java/com/gurjeet/codeitcapstone1project/adapter/UserAdapter.java
+++ app/src/main/java/com/gurjeet/codeitcapstone1project/adapter/UserAdapter.java
@@ -1,23 +1,23 @@
 package com.gurjeet.codeitcapstone1project.adapter;
 
-import android.content.Context;
-import android.content.Intent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.RecyclerView;
+        import androidx.annotation.NonNull;
+        import androidx.cardview.widget.CardView;
+        import androidx.recyclerview.widget.RecyclerView;
 
-import com.gurjeet.codeitcapstone1project.R;
-import com.gurjeet.codeitcapstone1project.model.UserRegister;
+        import com.gurjeet.codeitcapstone1project.R;
+        import com.gurjeet.codeitcapstone1project.model.UserRegister;
 
-import java.util.ArrayList;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     private ArrayList<UserRegister> UserList;
@@ -35,15 +35,80 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_user,parent,false);
-        return new ViewHolder(view);
+        View vi = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_user,parent,false);
+        return new ViewHolder(vi);
 
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        UserRegister usr = UserList.get(position);
+        holder.textView.setText(usr.getName());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                UserRegister usr = UserList.get(position);
 
+                final String ns = usr.getId();
+                String  username = usr.getName();
+                Toast.makeText(context, ""+ns, Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Username = "+username, Toast.LENGTH_LONG).show();
+
+                /* //to add any intent
+                Intent intennt = new Intent(v.getContext(), Message.class);
+                intennt.putExtra("id",ns);
+                intennt.putExtra("title",username);
+                context.startActivity(intennt);
+                */
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return UserList.size();
+    }
+    /*@Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String charSequenceString = constraint.toString();
+                if (charSequenceString.isEmpty()) {
+                    UserList = names;
+                } else {
+                    List<Name> filteredList = new ArrayList<>();
+                    for (Name name : nameList) {
+                        if (name.getName().toLowerCase().contains(charSequenceString.toLowerCase())) {
+                            filteredList.add(name);
+                        }
+                        filteredNameList = filteredList;
+                    }
+                }
+                FilterResults results = new FilterResults();
+                results.values = filteredNameList;
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                filteredNameList = (List<Name>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }*/
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-       //TODO: view holder data to be add here
+        ImageView imageView;
+        TextView textView;
+        CardView cardView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageuser);
+            textView = itemView.findViewById(R.id.textuser);
+            cardView =itemView.findViewById(R.id.cardview);
+        }
     }
 }
