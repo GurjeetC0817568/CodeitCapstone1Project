649efd10f3e17436a12bc278ca257ddb6263e58b
Gurjeet Kj <gurjeet-kj>
2021-11-25 14:16:23 -0500

    Done - Adding product: SellFragment file with FireStore to save data & StorageRefernce to store image in cloud

--- app/src/main/java/com/gurjeet/codeitcapstone1project/SellFragment.java
+++ app/src/main/java/com/gurjeet/codeitcapstone1project/SellFragment.java
@@ -1,64 +1,291 @@
 package com.gurjeet.codeitcapstone1project;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SellFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.gurjeet.codeitcapstone1project.model.PostModel;
+import com.squareup.picasso.Picasso;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
+
 public class SellFragment extends Fragment {
+    android.widget.Spinner spinner;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    SpinnerAdapter spinnerAdapter;
+    private double latitude,longitude;
+    ImageView imageView;
+    Button next,save;
+    RadioButton rbtnnew,rbtnused;
+    private static final int PICK_IMAGE  = 1;
+    private static final int PICK_MULTIPLE_IMAGE  = 2;
+    LocationManager locationManager;
+    TextInputLayout productname, location,numbe,pric;
+    RadioGroup radioGroup;
+    Bitmap bitmaps;
+    int Postion;
+    String ne,us,price,number,nam,n,p;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
+    private CollectionReference collectionReference = db.collection("data");
+    private Uri imageUri;
 
-    public SellFragment() {
-        // Required empty public constructor
-    }
+    FragmentManager fragmentManager;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SellFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SellFragment newInstance(String param1, String param2) {
-        SellFragment fragment = new SellFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sell, container, false);
+        //   ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Sell");
+        View view = connectViews(inflater, container);
+        final RadioGroup radio = (RadioGroup) getActivity().findViewById(R.id.radioG);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                View radioButton = radioGroup.findViewById(checkedId);
+                int index = radioGroup.indexOfChild(radioButton);
+
+                // Add logic here
+
+                switch (index) {
+                    case 0: // first button
+                        ne = rbtnnew.getText().toString();
+                        Toast.makeText(getActivity(), ""+ne,Toast.LENGTH_LONG).show();
+
+                        break;
+                    case 1: // secondbutton
+                        us = rbtnused.getText().toString();
+                        Toast.makeText(getActivity(), ""+us , Toast.LENGTH_LONG).show();
+
+                        break;
+                }
+            }
+        });
+
+        return view;
+
     }
+
+    private View connectViews(LayoutInflater inflater, ViewGroup container) {
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseAuth =FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        View view = inflater.inflate(R.layout.fragment_sell, container, false);
+        // spinner = view.findViewById(R.id.spinner);
+     //   location = view.findViewById(R.id.location);
+        imageView = view.findViewById(R.id.PimageView);
+     //   next = view.findViewById(R.id.btnnext);
+        save = view.findViewById(R.id.btnsave);
+
+        productname = view.findViewById(R.id.productname);
+        rbtnnew = view.findViewById(R.id.rbtnnew);
+        rbtnused = view.findViewById(R.id.rbtnused);
+        radioGroup = view.findViewById(R.id.radioG);
+
+        numbe = view.findViewById(R.id.NumberDialog);
+        pric  = view.findViewById(R.id.PriceDialog);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+            }
+        });
+        authStateListener= new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user!=null){
+
+                }else {
+
+                }
+            }
+        };
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //upload single image
+            public void onClick(View v) {
+                Intent Gallery = new Intent(Intent.ACTION_PICK);
+                Gallery.setType("image/*");
+                Gallery.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(Gallery, "Select Image"), PICK_IMAGE);
+
+            }
+
+        });
+
+
+
+
+
+
+        //Save part
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                number = numbe.getEditText().getText().toString();
+                price = pric.getEditText().getText().toString();
+                nam = productname.getEditText().getText().toString();
+                final String cm = ne;
+                final String um =us;
+                if (!TextUtils.isEmpty(nam)&&imageUri!=null){
+                    final StorageReference filepath = storageReference
+                            .child("Item_Images")
+                            .child("myimage"+ Timestamp.now().getSeconds());
+                    filepath.putFile(imageUri)
+                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+                                            String imageUri =  uri.toString();
+                                            final PostModel mod = new PostModel();
+                                            mod.setUserid(user.getUid());
+                                            mod.setName(nam);
+                                            if (rbtnnew.isChecked()){
+                                                mod.setCondition(ne);
+                                            }else{
+                                                mod.setCondition(us);
+                                            }
+                                            mod.setNumber(number);
+                                            //   mod.setPrice(Integer.parseInt(price));
+                                            try{
+                                                mod.setPrice(price);
+                                            } catch(NumberFormatException ex){ // handle your exception
+                                                Toast.makeText(getActivity(), "" + ex, Toast.LENGTH_SHORT).show();
+                                            }
+                                            mod.setImageURi(imageUri);
+                                            mod.setTimeAdd(new Timestamp(new Date()));
+                                            collectionReference.add(mod).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                @Override
+                                                public void onSuccess(DocumentReference documentReference) {
+                                                    Toast.makeText(getActivity(), "Data inserted", Toast.LENGTH_SHORT).show();
+                                                    Log.d("DocReference gk:",documentReference.toString());
+                                                    Toast.makeText(getActivity(), "name " + number + price ,  Toast.LENGTH_SHORT).show();
+                                                    HomeFragment home = new HomeFragment();
+                                                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                                                    transaction.replace(R.id.fragmentMainPart,home);
+                                                    transaction.commit();
+
+                                                    // Intent inten = new Intent(getActivity(),Home.class);
+                                                    //startActivity(inten);
+
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(getActivity(), "Error"+e.toString(), Toast.LENGTH_SHORT).show();
+                                                    Log.d("FailureMsg gk:",e.toString());
+
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getActivity(), "Error"+e.toString(), Toast.LENGTH_SHORT).show();
+                            Log.d("Error",e.toString());
+
+                        }
+
+                    });
+
+                }
+            }
+        });
+
+        return view;
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //  final Uri uri = data.getData();
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK){
+            if (data!=null){
+                imageUri = data.getData();
+                imageView.setImageURI(imageUri);
+            }
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        user =firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+//        Log.d("user",user.toString());
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (firebaseAuth!=null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
 }
